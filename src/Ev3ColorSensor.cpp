33f201857d94c8020a0330c9c85bb9b3d1e63040
theo-al <emailtheoprauerj@gmail.com>
2025-09-12 21:33:41 -0300

    Formatting and style changes

--- src/Ev3ColorSensor.cpp
+++ src/Ev3ColorSensor.cpp
@@ -1,66 +1,71 @@
-#include "Arduino.h"
-#include "SoftwareSerial.h"
-#include "Ev3ColorSensor.h"
+#include <assert.h>
 
-void Ev3ColorSensor::begin(){
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <Ev3ColorSensor.h>
+
+void Ev3ColorSensor::begin() {
   sensorSerial.begin(2400);
-  while (true) {
+
+  for(;;) {
     int value = sensorSerial.read();
     if (value != -1) {
       serialValue[1] = serialValue[0];
       serialValue[0] = value;
     }
 
-    if (serialValue[0] == 128 && serialValue[1] == 144) {
-      while (sensorSerial.read() != 4);
-      sensorSerial.write(ACK);
-      delay(60);
-      sensorSerial.end();
-      sensorSerial.begin(57600);
-      break;
-    }
+    if (serialValue[0] == 128 &&
+        serialValue[1] == 144) break;
   }
+  while (sensorSerial.read() != 4);
+
+  sensorSerial.write(ACK); delay(60);
+  sensorSerial.end();
+  sensorSerial.begin(57600);
   sendMode();
 }
 
-int8_t Ev3ColorSensor::read(){
+Ev3Color Ev3ColorSensor::read() {
   sendMode();
-  T0 = millis();
-  while(millis() - T0 <= waitTime); // ensures sensor is ready
-  int8_t resp;
+
+  // ensures sensor is ready
+  for(unsigned long t0 = millis(); millis() - t0 <= waitTime; );
+
+  Ev3Color resp;
   while (sensorSerial.available()){
     serialValue[2] = sensorSerial.read();
 
     if (serialValue[2] != -1 && serialValue[3] == 0) {
-      if((mode == COLOR) && serialValue[2] <= 7){ // color
+      if((mode == COLOR) && (serialValue[2] <= 7)){ // color
         serialValue[3] = 1;
         resp = serialValue[2];
-      }
-      else if((mode == RED_LIGHT || mode == BLUE_LIGHT) && serialValue[2] <= 100){ // light red or blue
+      } else if((mode == RED_LIGHT || mode == BLUE_LIGHT) &&
+                (serialValue[2] <= 100)) { // light red or blue
         serialValue[3] = 1;
         resp = serialValue[2];
-      }
-      else if(mode == OFF){ // off
+      } else if(mode == OFF) { // off
         serialValue[3] = 1;
         resp = 0;
       }
-    }
-    else {
+    } else {
       serialValue[3] = 0;
     }
   }
-  if (mode == COLOR && (resp < 0 || resp > 7))
-    resp = 0;
+  if ((mode == COLOR) && (resp < 0 || resp > 7)) resp = 0;
   return resp;
 }
 
-void Ev3ColorSensor::setMode(uint8_t newMode){
-  if (2 >= newMode && newMode >= 0 || newMode == 5)
-    mode = newMode;
+void Ev3ColorSensor::setMode(enum Ev3ColorSensorMode newMode) { 
+  switch(newMode) {
+    case RED_LIGHT: case BLUE_LIGHT: case COLOR: case OFF: {
+      mode = newMode; 
+    } break;
+
+    default: assert(!"unreachable");
+  }
 }
 
 void Ev3ColorSensor::sendMode() {
-
   sensorSerial.write(0x44);
   sensorSerial.write(0x11);
   sensorSerial.write(0xff ^ 0x44 ^ 0x11);
--- src/Ev3ColorSensor.h
+++ src/Ev3ColorSensor.h
@@ -3,34 +3,44 @@
   Version: 1.0
   Library made for use Ev3 color sensor with arduino
 
-  This library is distributed under MIT lisence in the hope
+  This library is distributed under MIT license in the hope
   that it will be useful, but WITHOUT ANY WARRANTY;
 */
 
 #ifndef EV3COLORSENSOR_H
 #define EV3COLORSENSOR_H
 
-#include "Arduino.h"
-#include "SoftwareSerial.h"
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
 #define ACK 0x04
 #define NACK 0x02
 
-// Modes define
-#define RED_LIGHT 0
-#define BLUE_LIGHT 1
-#define COLOR 2
-#define OFF 5
+enum Ev3ColorSensorMode : uint8_t {
+    RED_LIGHT  = 0,
+    BLUE_LIGHT = 1,
+    COLOR      = 2,
+    OFF        = 5,
+};
 
-class Ev3ColorSensor
-{
+enum Ev3Color : int8_t {
+    None   = 0,
+    Black  = 1,
+    Blue   = 2,
+    Green  = 3,
+    Yellow = 4,
+    Red    = 5,
+    White  = 6,
+    Brown  = 7,
+};
+
+class Ev3ColorSensor {
   private:
     int serialValue [4];
     uint8_t mode, waitTime;
-    unsigned long T0;
     SoftwareSerial sensorSerial;
     void sendMode();
   public:
-    // 
     Ev3ColorSensor(uint8_t rx, uint8_t tx, uint8_t wt = 5, uint8_t newMode = COLOR): 
       sensorSerial (rx, tx), mode (newMode), serialValue {0,0,0,0}, waitTime (wt){};
     
@@ -38,22 +48,14 @@ class Ev3ColorSensor
     void begin();
     
     // Returns a color number |
-    // 0	No color |
-    // 1	Black |
-    // 2	Blue |
-    // 3	Green |
-    // 4	Yellow |
-    // 5	Red |
-    // 6	White |
-    // 7	Brown |
-    int8_t read();
+    Ev3Color read();
 
     // Accepts a defined constant |
     // RED_LIGHT |
     // BLUE_LIGHT |
     // COLOR_LIGHT |
     // OFF |
-    void setMode(uint8_t newMode);
+    void setMode(Ev3ColorSensorMode newMode);
 
     // Changes which sensor will be read
     void activate();
